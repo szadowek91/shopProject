e524e708090071905e39e41dcb1528d76caa14f8
PG <szadowek91@gmail.com>
2022-03-17 07:38:48 +0100

    Initial commit

---
+++ src/test/java/pl/szadowek91/shopProject/ShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szadowek91.shopProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

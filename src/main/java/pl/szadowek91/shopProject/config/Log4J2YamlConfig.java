72aeb8f5c52aa7c52ec5b5a05940c8f2e5c1242f
PG <szadowek91@gmail.com>
2022-05-06 08:17:28 +0200

    app properties configuration , loggers configuration , db configuration added.

---
+++ src/main/java/pl/szadowek91/shopProject/config/Log4J2YamlConfig.java
@@ -0,0 +1,33 @@
+package pl.szadowek91.shopProject.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4J2YamlConfig {
+
+    private Logger logger = LogManager.getLogger();
+
+    public void performSomeTask() {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+    }
+
+    public void infoLogEnterIntoMethod(String methodName) {
+        logger.info("Enter into " + methodName + " method.");
+    }
+
+    public void infoLogSuccess() {
+        logger.info("Done successfully.");
+    }
+
+    public void errorLog(String errorName) {
+        logger.error("something goes wrong ! : " + errorName);
+    }
+
+    public void fatalLog(String fatalErrorName) {
+        logger.fatal(fatalErrorName + "  == FATAL ERROR, contact with support: 666-666-666");
+    }
+}
